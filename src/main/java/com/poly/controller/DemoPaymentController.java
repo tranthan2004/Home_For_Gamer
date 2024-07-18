888fb7feafb2113afe5a538115481c6c4dc617aa
ChiHieu <chihieu2402@gmail.com>
2024-07-18 13:47:52 +0700

    add file index.html and DemoPaymentController

---
+++ src/main/java/com/poly/controller/DemoPaymentController.java
@@ -0,0 +1,15 @@
+package com.poly.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class DemoPaymentController {
+	@RequestMapping(value="/payment", method=RequestMethod.GET)
+	public String pay() {
+		return "index";
+	}
+	
+}
